e95c51f3c867ef36f2cd20f021831fa5b1dbecaa
Mraakash005 <145696220+Mraakash005@users.noreply.github.com>
2023-10-05 18:34:28 +0530

    Regular hexagon

---
+++ file3.c
@@ -0,0 +1,12 @@
+// WAP in c to calculate a regular hexagon //
+
+
+#include<stdio.h>
+int main (){
+    float area ,d,a;
+    printf("ENTER THE LENGHT OF SIDE:");
+    scanf("%f",&d);
+    area=3*1.73*a*a/2;
+    printf("Area of hexagon=%f",area);
+}
+
