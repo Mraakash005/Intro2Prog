03b4293332b619784cf958fba28f1cdde6e29617
Mraakash005 <145696220+Mraakash005@users.noreply.github.com>
2023-10-16 22:49:22 +0530

    WAP to display your five best friend using function

---
+++ file 39.c
@@ -0,0 +1,14 @@
+//Write a program in c to display the name of your five best friends using functions.
+#include<stdio.h>
+void name()
+{
+    printf("Ashish singh\n");
+    printf("Satyam chauhan\n");
+    printf("Sahil negi\n");
+    printf("Akshsay kumar\n");
+    printf("Abhijeet paswan\n");
+}
+int main()
+{
+    name();
+}
