72d9656689ee4033f5bf3e7d45652ae4357dbfc6
Mraakash005 <145696220+Mraakash005@users.noreply.github.com>
2023-11-21 22:21:28 +0530

    File handling

---
+++ File54.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    FILE *fp;
+    char data[100];
+    fp=fopen("File54.c","r");
+
+    if(fp!=NULL){
+        printf("FILE OPENED !");
+        fgets(data,100,fp);
+        printf("%s",data);
+    }
+    else{
+        printf("error !");
+    }
+}
