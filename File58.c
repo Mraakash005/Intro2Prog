54dc1744098642fef9a6e69f4504a308ae308872
Mraakash005 <145696220+Mraakash005@users.noreply.github.com>
2023-12-10 18:18:53 +0530

    WAP in to delete an element from an array

--- File58.c
+++ File58.c
@@ -1,18 +1,19 @@
+    //DELETION OF AN ELEMENT FROM AN ARRAY.
+
 #include<stdio.h>
-int main(){
-
-    int main[5]={10,15,2,4,7};
-    int index;
-    int data;
-
-    for(int i=0;i<=5;i++){
-        printf("%d",data[i]);
+    int main() {
+        int data[5] = {10, 5, 20, 44, 69};
+        int index;
+        for (int i = 0; i < 5; i++) {
+            printf("%d   ", data[i]);
+        }
+        printf("\n ENTER THE INDEX POSITION WHOSE VALUE IS TO BE DELETED:\n");
+        scanf("%d", &index);
+        for (int i = index; i < 5; i++) {
+            data[i] = data[i + 1];
+        }
+        for (int i = 0; i < 4; i++) {
+            printf("%d ", data[i]);
+        }
     }
-    printf("\nENTER THE INDEX POSITION WHOSE VALUE IS TO BE DELETED !\n");
-    scanf("%d",&index);
 
-    for(int i=index;i<5;i++){
-        data[i]=data[i+1];
-    }
-
-}
